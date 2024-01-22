3e3df2eb3cfb5e5f65b899e412deeb90106d8e8e
Abdulla Alsatrawi <Abdulla.Alsatrawi@gmail.com>
2024-01-22 16:32:39 +0300

    spaces only

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ b.addEventListener('click', () =>{
     })
 })
 
+/////////////////////////////////////////////////////////////////////     
+
+colorMatching 
+
 
 
 // const changeColor = () => {
